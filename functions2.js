d87dda3d918d61241632c62a847a401edd7841f7
KimSteenBekke <KimSteenBekke@lego.com>
2021-10-25 22:17:43 +0200

    functions

---
+++ functions2.js
@@ -0,0 +1,36 @@
+// Create custom functions with parameters //
+
+// function "dieSize" defines the number of eyes on the dies through parameters on "var firstDie = getDieRoll1 (10); and "var secondDiee = getDieRoll2 (10);" 
+
+var getDieRoll1 = function(dieSize) {
+
+    console.log(dieSize);
+    return Math.ceil (dieSize * Math.random () );
+
+    };
+
+ var getDieRoll2 = function(dieSize) {
+
+    console.log(dieSize);
+    return Math.ceil (dieSize * Math.random () );
+  
+    };
+
+var y = "diesize 1:";
+console.log(y);
+var firstDie = getDieRoll1 (10);
+var yy = "eyes die 1:";
+console.log(yy);
+console.log(firstDie);
+
+
+var z = "diesize 2:";
+console.log(z);
+var secondDie = getDieRoll2 (10);
+var zz = "eyes die 2:";
+console.log(zz);
+console.log(secondDie);
+
+var x = "total eyes:";
+console.log(x);
+console.log(firstDie + secondDie);
